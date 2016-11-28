60d00516fc3fcb548c06f5a08fc7e116c868e4ad
fabio@fabiofarias.com.br <Pernalonga.12>
2016-11-28 00:01:25 -0200

    All

---
+++ jasmine-standalone-2.5.2/spec/MaiorEMenorSpec.js
@@ -0,0 +1,45 @@
+/**
+ * Created by fabiorocha on 27/11/16.
+ */
+
+describe("MaiorEMenor", function () {
+
+    it("deve entender numeros em rdem nao sequencial", function () {
+
+        var algoritimo = new MaiorEMenor();
+
+        algoritimo.encontra([5,15,7,9]);
+
+        expect(algoritimo.pegaMaior()).toEqual(15);
+        expect(algoritimo.pegaMenor()).toEqual(5);
+
+
+    });
+
+
+    it("deve entender numeros em ordem crescente", function () {
+
+        var algoritimo = new MaiorEMenor();
+
+        algoritimo.encontra([5,6,7,8]);
+
+        expect(algoritimo.pegaMaior()).toEqual(8);
+        expect(algoritimo.pegaMenor()).toEqual(5);
+
+
+    });
+
+    it("deve entender numeros em ordem decrescente", function () {
+
+        var algoritimo = new MaiorEMenor();
+
+        algoritimo.encontra([8,7,6,5]);
+
+        expect(algoritimo.pegaMaior()).toEqual(8);
+        expect(algoritimo.pegaMenor()).toEqual(5);
+
+
+    });
+
+
+})
